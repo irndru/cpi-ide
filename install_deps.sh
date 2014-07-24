3a29a708f38b001b3f336df34c52c4b3e06acb9d
McCraeAndrew <andrew.a.mccrae@gmail.com>
2014-07-24 11:57:38 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 sudo apt-get update
-sudo apt-get -y install ghc libncurses5-dev happy alex libcairo2-dev libpango1.0-dev gtk+-3.0 glib-2.0 gsl-bin gsl-ref-html libgsl0-dev liblapack-dev libgtksourceview-3.0 libgtksourceview-dev-3.0 cabal-install 
+sudo apt-get install ghc libncurses5-dev happy alex libcairo2-dev libpango1.0-dev gtk+-3.0 glib-2.0 gsl-bin gsl-ref-html libgsl0-dev liblapack-dev libgtksourceview-3.0 libgtksourceview-3.0-dev cabal-install
 cabal update
-cabal install gtk2hs-buildtools --global
-cabal install text hledger gtk3 MissingH hmatrix data-accessor colour Chart Chart-cairo hmatrix-gsl gtksourceview3
+sudo cabal install --global gtk2hs-buildtools text hledger gtk3 MissingH hmatrix data-accessor colour Chart Chart-cairo hmatrix-gsl gtksourceview3
